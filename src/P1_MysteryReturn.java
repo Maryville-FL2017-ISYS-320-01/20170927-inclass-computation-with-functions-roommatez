e40b284f2bda18f9c1ec97a29ae53382f1ef1217
jmeurer1 <jmeurer1@live.maryville.edu>
2017-09-27 15:26:08 -0600

    InClass Computation

--- src/P1_MysteryReturn.java
+++ src/P1_MysteryReturn.java
@@ -6,16 +6,35 @@
 
 /*
  1. Your output prediction (do yourself a favor and go step by step through the code, recording the value of each variable at each method call):
-  
-  
+  // We had no idea where to start with guessing, so we ended up running the program and working backwards through the problem.
+   * 
+  //Notes on how to do it for future use: on each line of code, the vars in the () up top are actually assigned in the same order that they are shown but at the bottom,
+   * so on the first line, z = mystery (x,z,y) is actually having x=z, z=x, and y=y in the bottom function.
  */
 
 /*
  3. Were you correct? Explain if you were not (what did you learn?)
- 
+ We were not correct because we did not have an educated guess as to what to do.
  
  */
 public class P1_MysteryReturn {
+	public static void main(String[] args) {
+        int x = 1, y = 2, z = 3;
+        z = mystery( x, z, y);
+        System.out.println( x + " " + y + " " + z);
+        x = mystery( z, z, x);
+        System.out.println( x + " " + y + " " + z);
+        y = mystery( y, y, z);
+        System.out.println( x + " " + y + " " + z);
 
+    }
+    
+    public static int mystery( int z, int x, int y) {
+        z--;
+        x = 2 * y + z;
+        y = x - 2;
+        System.out.println(y + " " + z );
+        return x;
+    }
 
 }
--- src/P4_FixTempConverter.java
+++ src/P4_FixTempConverter.java
@@ -6,7 +6,7 @@
 
 /*
  What were the errors you found?
- 
+ // Can't return a value with void in the header, println was in the wrong spot, and had to add a return tempc
  
  */
 
@@ -15,11 +15,13 @@ public class P4_FixTempConverter {
 		double tempf = 98.6;
 		double tempc = 0.0;
 		ftoc(tempf, tempc);
-		System.out.println("Body temp in C is: " + tempc);
+		
 	}
 
 	// converts Fahrenheit temperatures to Celsius
-	public static void ftoc(double tempf, double tempc) {
+	public static double ftoc(double tempf, double tempc) {
 	      tempc = (tempf - 32) * 5 / 9;
+	      System.out.println("Body temp in C is: " + tempc);
+	      return tempc;
     }
 }
