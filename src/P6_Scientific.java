bd6a41d5591af29555263b2528134cc966dc1a3c
jmeurer1 <jmeurer1@live.maryville.edu>
2017-09-28 23:27:42 -0600

    Computation

--- src/P6_Scientific.java
+++ src/P6_Scientific.java
@@ -8,7 +8,10 @@ public class P6_Scientific {
 
 	public static void main(String[] args) {
 		// REMINDER: Any print statements should only be in the main method
-
+		System.out.println(ComputeValue(6.5, 3));
 	}
-
+	public static double ComputeValue(double base, double exponent) {
+		double value = base * (Math.pow(10,  exponent));
+		return value;
+	}
 }
